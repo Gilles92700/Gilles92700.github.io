dde06b6b344d09d6ca38695a0377d92bbab4848c
Gilles92700 <ggirault@9online.fr>
2015-02-26 17:10:56 +0100

    26-02-2015
    Suppressiojn top_menu.php

--- top_menu.php
+++
@@ -1,9 +0,0 @@
-  <nav id="top_menu">
-		<div class="top_menu">
-			<ul>
-				<li><a href="#">Mentions Légales</a></li>
-				<li><a href="http://gilles92700.github.io/contact.html">Contact</a></li>
-				<li><a href="#">Formations</a></li>
-			</ul>
-		</div>	
-	</nav>
